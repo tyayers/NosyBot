3920d4b6c20541802134a4b99dee7bc10bae9115
Tyler Ayers <tyler.ayers@gmail.com>
2018-07-17 22:35:02 +0200

    changed translation to v3

---
+++ NosyBot.Services/Dtos/TranslateCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NosyBot.Services.Dtos
+{
+    public class TranslateCommand
+    {
+        public string DisplayLanguage { get; set; }
+        public Dictionary<string, Translate> Translations { get; set; } = new Dictionary<string, Translate>();
+
+        public void AddStory(StoryRecord story)
+        {
+            if (!Translations.ContainsKey(story.Language))
+            {
+                Translations.Add(story.Language, new Translate() { LangaugeFrom = story.Language, LanguageTo = DisplayLanguage });                
+            }
+
+            Translations[story.Language].Stories.Add(story);
+        }
+    }
+
+    public class Translate
+    {
+        public string LangaugeFrom { get; set; }
+        public string LanguageTo { get; set; }
+        public List<StoryRecord> Stories { get; set; } = new List<StoryRecord>();
+    }
+}
